0c941a296404819e0967dda9cc661cab0ffd6f5a
Kazi Sabbir Ahmed Opi <ahmedsabbir401@gmail.com>
2023-03-29 00:53:44 +0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });   
+    print(questionIndex);
+  }
+
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animel?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text('Answer 1'), onPressed: () => print('Answer 3')),
+          ],
+        ),
       ),
     );
   }
